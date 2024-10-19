e9a1084f021d29e1e021fef0905fc67fcf2ee7a5
login-nigol <vadim.antipov@gmx.de>
2024-10-19 08:16:16 +0200

    обновлён

--- src/main/java/example/functionality/EvenSum.java
+++ src/main/java/example/functionality/EvenSum.java
@@ -3,7 +3,7 @@ package example.functionality;
 import java.util.Arrays;
 import java.util.List;
 
-public class EvenSum {
+public class EvenSum { // нахождение суммы чётных чисел
     
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
